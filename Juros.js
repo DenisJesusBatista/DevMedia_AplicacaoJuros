ec03dd1c5ba93314d9aacd36b160589fffafbe27
Denis Batista <denis.si2011@gmail.com>
2022-01-27 18:52:24 -0300

    FIX: Passo 4 - Processamento de dados: Cálculo

--- Juros.js
+++ Juros.js
@@ -2,10 +2,30 @@ import entradaDados from 'readline-sync';
 
 console.log('Aplicação de Juros: ');
 
+let taxa_Juros = 10;
+
+let valor_divida = 100;
+
 let valor_divida = entradaDados.question("\nInforme o valor devido:  R$");
+
 let dias_vencimento = entradaDados.question("\nInforme quantos dias se passaram desde o vencimento do boleto:");
+
+valor_divida = Number(valor_divida);
+dias_vencimento = Number(dias_vencimento);
+
+let valor_Juros = (valor_divida / 100) * taxa_Juros;
+let totalDivida = valor_divida + valor_Juros;
 
 console.log("\nValor original da dívida: R$ " + valor_divida);
 console.log("\nDias atrasados: " + dias_vencimento);
-console.log("Taxa de Juros: 0%");
-console.log("Valor total com juros: R$ 0");
+console.log("Taxa de Juros: " + taxa_Juros);
+console.log("Valor total com juros: R$ " + totalDivida);
+
+
+let valorDivida = 100;
+let juros = 100;
+
+let valorJuros = (valorDivida / 100) * juros;
+let totalDivida = valorDivida + valorJuros;
+
+console.log(totalDivida);
