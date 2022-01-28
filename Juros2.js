c1a6af510134c4489ed219d8d6e7752b65cdec8b
Denis Batista <denis.si2011@gmail.com>
2022-01-28 19:13:38 -0300

    FIX: Validacao de dados - Concluido missão - Construindo uma aplicação de juros

--- Juros2.js
+++ Juros2.js
@@ -2,11 +2,25 @@ import entradaDados from 'readline-sync';
 
 console.log('Aplicação de Juros: ');
 
-let taxa_Juros = 0;
+let taxa_Juros;
 
 let valor_divida = entradaDados.question("\nInforme o valor devido:  R$");
 
+if (valor_divida <= 0) {
+    console.log("\nValor original da dívida: R$ " + valor_divida);
+    console.log("O valor da dívida deve ser maior que zero!\n");
+} else {
+    console.log("\nValor original da dívida: R$ " + valor_divida);
+}
+
+
 let dias_vencimento = entradaDados.question("Informe dias se passaram do vencimento: ");
+if (dias_vencimento <= 0) {
+    console.log("\nDias atrasados: " + dias_vencimento);
+    console.log("Você está em dia!\n");
+} else {
+    console.log("\nDias atrasados: " + dias_vencimento);
+}
 
 
 
@@ -23,7 +37,8 @@ let totalDivida = Number(valor_divida) + Number(valor_Juros);
 
 
 
-console.log("\nValor original da dívida: R$ " + valor_divida);
-console.log("Dias atrasados: " + dias_vencimento);
+
+
+// console.log("Dias atrasados: " + dias_vencimento);
 console.log("Taxa de Juros: " + taxa + "%");
 console.log("Valor total com juros: R$ " + totalDivida);
